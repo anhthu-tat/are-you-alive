0572e3da53ca8996b0d2a5891c594cdd1c246020
anhthu-tat <10423173@student.vgu.edu.vn>
2025-06-10 12:44:12 +0000

    Complete

--- youtube/codebeauty.cpp
+++ youtube/codebeauty.cpp
@@ -2,13 +2,28 @@
 using namespace std;
 
 class Employee {
+    //all attributes of class is private by default
+    //must make public
+public:
     string Name;
-    string Compnay;
+    string Company;
     int Age;
+
+    //a class method aka a function
+    void IntroduceYourself() {
+            cout << "Name - " << Name << endl;
+            cout << "Company - " << Company << endl;
+            cout << "Age - " << Age << endl;
+    }
 };
 
 
 int main()
 {
+    Employee employee1;
+    employee1.Name = "TAT";
+    employee1.Company = "Catkery";
+    employee1.Age = 20;
+    employee1.IntroduceYourself();
 
 }
